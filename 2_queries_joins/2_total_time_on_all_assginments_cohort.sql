9dcb374ac4b50c365801db0fdf2f82b29c8ceb9f
Linuxe <angelren1220@gmail.com>
2023-04-03 19:22:20 -0400

    delete trial code

--- 2_queries_joins/2_total_time_on_all_assginments_cohort.sql
+++ 2_queries_joins/2_total_time_on_all_assginments_cohort.sql
@@ -1,11 +1,3 @@
--- SELECT SUM(duration) AS total_duration
--- FROM (
---   SELECT assignment_submissions.duration, students.cohort_id
---   FROM assignment_submissions
---   JOIN students ON students.id = assignment_submissions.student_id
--- ) AS temp
--- JOIN cohorts ON students.cohort_id = cohorts.id
--- WHERE cohort.name = 'FEB12';
 SELECT SUM(assignment_submissions.duration) AS total_duration
 FROM
   assignment_submissions
