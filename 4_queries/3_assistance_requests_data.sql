ea8a874289846f2f4a5685289fcb91b0a6f1911d
Linuxe <angelren1220@gmail.com>
2023-04-04 21:27:41 -0400

    fix the missing rows bug

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -4,5 +4,6 @@ assignments.name AS assignment, (completed_at-started_at) AS duration
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
-JOIN assignments ON assignments.id = assignment_id
+--use left join since there are some assistance requests are not about assignments, their assginment.id is null ---
+LEFT JOIN assignments ON assignments.id = assignment_id
 ORDER BY duration;
